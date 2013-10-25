b9a8d95f199526111ed120b01599fff20cb1f298
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-10-24 23:04:04 -0400

    refactored a bit, and cleanup enemy movement tiles

--- public/js/enemy_turn.js
+++ public/js/enemy_turn.js
@@ -8,36 +8,44 @@ var EnemyTurn = Turn.extend({
     },
 
     update: function () {
-        var firstEnemy = _.first(_.where(this.objects.where({enemyControlled: true}), {disabled: false}));;
-        if (firstEnemy) {
-            this.moveUnit = new MoveUnit(this.objects, {enemyControlled: true}, {enemyCannotMoveThrough: true}, RED_TILES);
-            this.moveUnit.createMovementTiles(firstEnemy.position);
+        var enemy = this.enemyToUpdate();
+        if (!enemy)
+            return;
 
-            var player = firstEnemy.isNextToAny(this.objects.where({enemyAttackable: true}));
+        this.moveUnit = new MoveUnit(this.objects, {enemyControlled: true}, {enemyCannotMoveThrough: true}, RED_TILES);
+        this.moveUnit.createMovementTiles(enemy.position);
 
-            if (player)
-                this.moveUnit.movePlayerIfClickedTile(firstEnemy.position);
-            else {
-                var movementTiles = this.objects.where({movementTile: true});
-                _.some(movementTiles, function (movementTile) {
-                    if (this.objects.where({enemyControlled: true}).isAtPosition(movementTile.position))
-                        return false;
+        var player = enemy.isNextToAny(this.objects.where({enemyAttackable: true}));
 
-				            if (movementTile.isNextToAny(this.objects.where({playerControlled: true}))) {
-                        this.moveUnit.movePlayerIfClickedTile(movementTile.position);
-                        player = firstEnemy.isNextToAny(this.objects.where({enemyAttackable: true}));
-                        return true;
-                    }
-                }, this);
-            }
-
-            if (player) {
-                new Battle(firstEnemy, player, this);
-            }
-            else {
-				        firstEnemy.disabled = true;
-            }
+        if (player) {
+            this.moveUnit.movePlayerIfClickedTile(enemy.position);
+            new Battle(enemy, player, this);
         }
+        else {
+            this.moveEnemyToPlayer(enemy);
+            this.objects.removeAll({movementTile: true});
+        }
+
+				enemy.disabled = true;
+    },
+
+    enemyToUpdate: function () {
+				return _.first(_.where(this.objects.where({enemyControlled: true}), {disabled: false}));;
+    },
+
+    moveEnemyToPlayer: function (enemy) {
+				var movementTiles = this.objects.where({movementTile: true});
+        _.some(movementTiles, function (movementTile) {
+            if (this.objects.where({enemyControlled: true}).isAtPosition(movementTile.position))
+                return false;
+
+				    if (movementTile.isNextToAny(this.objects.where({playerControlled: true}))) {
+                this.moveUnit.movePlayerIfClickedTile(movementTile.position);
+                var player = enemy.isNextToAny(this.objects.where({enemyAttackable: true}));
+                new Battle(enemy, player, this);
+                return true;
+            }
+        }, this);
     },
 
     unitTypes: function () {
