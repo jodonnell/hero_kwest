b4e27a17375505686a2874e73214190423f2deec
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-11-26 07:45:14 -0500

    whoops on descend level forgot to readd embellishments

--- public/js/game_controller.js
+++ public/js/game_controller.js
@@ -29,6 +29,7 @@ var GameController = Class.extend({
         this.objects.add(level.floors, {movableThrough: true, z: 100});
         this.objects.add(level.enemyUnits, this.objects.enemyUnit());
         this.objects.add(level.stairs, this.objects.stairs());
+        this.objects.add(level.shadows, {movableThrough: true, z: 1001});
 
         this.newPlayerTurn();
     },
